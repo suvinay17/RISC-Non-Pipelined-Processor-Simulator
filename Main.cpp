48420180e726935495cbe5ad0c8fe01847cc48e8
Suvinay Bothra <suvinay@Suvinays-MacBook-Pro.local>
2019-12-02 21:03:51 -0500

    main updates

--- Main.cpp
+++ Main.cpp
@@ -17,31 +17,52 @@
 #include "RegisterTable.h"
 #include "SignExtend.h"
 #include "SymbolTable.h"
+#include "Simulator.h"
 #include <stdio.h>
 #include <string>
 #include <fstream>
 #include <iostream>
 
-int main(int argc, char *argv[])
-{
-
-  ifstream inputFile;
-
-  if(argc < 2){
-    cerr << "Specify a configuration file to simulate the processr."<<endl;
-    return 0;
-  }
-
-  inputFile.open(argv[1]);
-    if (!inputFile.is_open()){
-        cerr << "Error: Unable to open file " << argv[1] << endl;
-        return 1;
-    }
-  
 
 
 
-};
+using namespace std;
+
+/* This file reads in a MIPS assembly file specified at the command line.
+ * If the file is correct syntactically, each instruction in the file
+ * will be translated into its 32 bit MIPS binary encoding and printed
+ * to stdout, one per line.
+ *
+ */
+
+ int main(int argc, char *argv[])
+ {
+   Simulator *parser;
+
+   if(argc < 2){
+     cerr << "Need to specify an assembly file to translate."<<endl;
+     exit(1);
+   }
+
+   parser = new Simulator(argv[1]);
+
+   if(parser->isFormatCorrect() == false){
+     cerr << "Format of input file is incorrect " << endl;
+     exit(1);
+   }
+
+   Instruction i;
+
+   //Iterate through instructions, printing each encoding.
+   i = parser->getNextInstruction();
+   while( i.getOpcode() != UNDEFINED){
+     // cout << i.getString() << endl;
+     cout << i.getEncoding() << endl;
+     i = parser->getNextInstruction();
+   }
+
+   delete parser;
+ }
 
 
 
